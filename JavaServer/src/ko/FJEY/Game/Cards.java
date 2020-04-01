9016dbe2a1bc304b679954ce1e9f7ac83c2542e3
FJEY <FJEY@DESKTOP-25B2QUS>
2020-04-02 03:22:35 +0900

    Hand Check Initial Commit

--- JavaServer/src/ko/FJEY/Game/Cards.java
+++ JavaServer/src/ko/FJEY/Game/Cards.java
@@ -4,83 +4,83 @@ public enum Cards {
 	
 	
 	
-	DIAMOND_1("DIAMOND", "A" , "d1"),
-	DIAMOND_2("DIAMOND", "2" , "d2"),
-	DIAMOND_3("DIAMOND", "3" , "d3"),
-	DIAMOND_4("DIAMOND", "4" , "d4"),
-	DIAMOND_5("DIAMOND", "5" , "d5"),
-	DIAMOND_6("DIAMOND", "6" , "d6"),
-	DIAMOND_7("DIAMOND", "7" , "d7"),
-	DIAMOND_8("DIAMOND", "8" , "d8"),
-	DIAMOND_9("DIAMOND", "9" , "d9"),
-	DIAMOND_10("DIAMOND", "10" , "d10"),
-	DIAMOND_J("DIAMOND", "J" , "dJ"),
-	DIAMOND_Q("DIAMOND", "Q" , "dQ"),
-	DIAMOND_K("DIAMOND", "K" , "dK"),
-	CLOVER_1("CLOVER", "A" , "c1"),
-	CLOVER_2("CLOVER", "2" , "c2"),
-	CLOVER_3("CLOVER", "3" , "c3"),
-	CLOVER_4("CLOVER", "4" , "c4"),
-	CLOVER_5("CLOVER", "5" , "c5"),
-	CLOVER_6("CLOVER", "6" , "c6"),
-	CLOVER_7("CLOVER", "7" , "c7"),
-	CLOVER_8("CLOVER", "8" , "c8"),
-	CLOVER_9("CLOVER", "9" , "c9"),
-	CLOVER_10("CLOVER", "10" , "c10"),
-	CLOVER_J("CLOVER", "J" , "cJ"),
-	CLOVER_Q("CLOVER", "Q" , "cQ"),
-	CLOVER_K("CLOVER", "K" , "cK"),
-	SPADE_1("SPADE", "A" , "s1"),
-	SPADE_2("SPADE", "2" , "s2"),
-	SPADE_3("SPADE", "3" , "s3"),
-	SPADE_4("SPADE", "4" , "s4"),
-	SPADE_5("SPADE", "5" , "s5"),
-	SPADE_6("SPADE", "6" , "s6"),
-	SPADE_7("SPADE", "7" , "s7"),
-	SPADE_8("SPADE", "8" , "s8"),
-	SPADE_9("SPADE", "9" , "s9"),
-	SPADE_10("SPADE", "10" , "s10"),
-	SPADE_J("SPADE", "J" , "sJ"),
-	SPADE_Q("SPADE", "Q" , "sQ"),
-	SPADE_K("SPADE", "K" , "sK"),
-	HEART_1("HEART", "A" , "h1"),
-	HEART_2("HEART", "2" , "h2"),
-	HEART_3("HEART", "3" , "h3"),
-	HEART_4("HEART", "4" , "h4"),
-	HEART_5("HEART", "5" , "h5"),
-	HEART_6("HEART", "6" , "h6"),
-	HEART_7("HEART", "7" , "h7"),
-	HEART_8("HEART", "8" , "h8"),
-	HEART_9("HEART", "9" , "h9"),
-	HEART_10("HEART", "10" , "h10"),
-	HEART_J("HEART", "J" , "hJ"),
-	HEART_Q("HEART", "Q" , "hQ"),
-	HEART_K("HEART", "K" , "hK"),
+	DIAMOND_A("DIAMOND", 14 , "dA"),
+	DIAMOND_2("DIAMOND", 2 , "d2"),
+	DIAMOND_3("DIAMOND", 3 , "d3"),
+	DIAMOND_4("DIAMOND", 4 , "d4"),
+	DIAMOND_5("DIAMOND", 5 , "d5"),
+	DIAMOND_6("DIAMOND", 6 , "d6"),
+	DIAMOND_7("DIAMOND", 7 , "d7"),
+	DIAMOND_8("DIAMOND", 8 , "d8"),
+	DIAMOND_9("DIAMOND", 9 , "d9"),
+	DIAMOND_10("DIAMOND", 10 , "d10"),
+	DIAMOND_J("DIAMOND", 11 , "dJ"),
+	DIAMOND_Q("DIAMOND", 12 , "dQ"),
+	DIAMOND_K("DIAMOND", 13 , "dK"),
+	CLOVER_A("CLOVER", 14 , "cA"),
+	CLOVER_2("CLOVER", 2 , "c2"),
+	CLOVER_3("CLOVER", 3 , "c3"),
+	CLOVER_4("CLOVER", 4 , "c4"),
+	CLOVER_5("CLOVER", 5 , "c5"),
+	CLOVER_6("CLOVER", 6 , "c6"),
+	CLOVER_7("CLOVER", 7 , "c7"),
+	CLOVER_8("CLOVER", 8 , "c8"),
+	CLOVER_9("CLOVER", 9 , "c9"),
+	CLOVER_10("CLOVER", 10 , "c10"),
+	CLOVER_J("CLOVER", 11 , "cJ"),
+	CLOVER_Q("CLOVER", 12 , "cQ"),
+	CLOVER_K("CLOVER", 13 , "cK"),
+	SPADE_A("SPADE", 14 , "sA"),
+	SPADE_2("SPADE", 2 , "s2"),
+	SPADE_3("SPADE", 3 , "s3"),
+	SPADE_4("SPADE", 4 , "s4"),
+	SPADE_5("SPADE", 5 , "s5"),
+	SPADE_6("SPADE", 6 , "s6"),
+	SPADE_7("SPADE", 7 , "s7"),
+	SPADE_8("SPADE", 8 , "s8"),
+	SPADE_9("SPADE", 9 , "s9"),
+	SPADE_10("SPADE", 10 , "s10"),
+	SPADE_J("SPADE", 11 , "sJ"),
+	SPADE_Q("SPADE", 12 , "sQ"),
+	SPADE_K("SPADE", 13 , "sK"),
+	HEART_A("HEART", 14 , "hA"),
+	HEART_2("HEART", 2 , "h2"),
+	HEART_3("HEART", 3 , "h3"),
+	HEART_4("HEART", 4 , "h4"),
+	HEART_5("HEART", 5 , "h5"),
+	HEART_6("HEART", 6 , "h6"),
+	HEART_7("HEART", 7 , "h7"),
+	HEART_8("HEART", 8 , "h8"),
+	HEART_9("HEART", 9 , "h9"),
+	HEART_10("HEART", 10 , "h10"),
+	HEART_J("HEART", 11 , "hJ"),
+	HEART_Q("HEART", 12 , "hQ"),
+	HEART_K("HEART", 13 , "hK"),
 	
-	DIAMOND("DIAMOND", "", ""),
-	CLOVER("CLOVER", "" ,""),
-	SPADE("SPADE", "", ""),
-	HEART("HEART", "", ""),
+	DIAMOND("DIAMOND", 0, ""),
+	CLOVER("CLOVER", 0 ,""),
+	SPADE("SPADE", 0, ""),
+	HEART("HEART", 0, ""),
 	
-	A("", "A",""),
-	_2("", "2",""),
-	_3("", "3",""),
-	_4("", "4",""),
-	_5("", "5",""),
-	_6("", "6",""),
-	_7("", "7",""),
-	_8("", "8",""),
-	_9("", "9",""),
-	_10("", "10",""),
-	J("", "J",""),
-	Q("", "Q",""),
-	K("", "K",""),
+	A("", 14,""),
+	_2("", 2,""),
+	_3("", 3,""),
+	_4("", 4,""),
+	_5("", 5,""),
+	_6("", 6,""),
+	_7("", 7,""),
+	_8("", 8,""),
+	_9("", 9,""),
+	_10("", 10,""),
+	J("", 11,""),
+	Q("", 12,""),
+	K("", 13,""),
 	
-	NO("","","");
+	NO("",0,"");
 	
 	final private String name;
 	final private String type;
-	final private String number;
+	final private int number;
 	
 	public String getName() {
 		return name;
@@ -90,7 +90,7 @@ public enum Cards {
 		return type;
 	}
 	
-	public String getNumber() {
+	public int getNumber() {
 		return number;
 	}
 	
@@ -102,7 +102,7 @@ public enum Cards {
 	}
 	
 	public Cards isSameType(Cards card) {
-		if(this.getType() == card.getType()) {
+		if(this.getType().equals(card.getType())) {
 			return this;
 		}
 		return NO;
@@ -115,7 +115,7 @@ public enum Cards {
 		return NO;
 	}
 	
-	private Cards(String type, String number, String name) {
+	private Cards(String type, int number, String name) {
 		this.type = type;
 		this.number = number;
 		this.name = name;
--- JavaServer/src/ko/FJEY/Game/GameManager.java
+++ JavaServer/src/ko/FJEY/Game/GameManager.java
@@ -1,6 +1,6 @@
 package ko.FJEY.Game;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GameManager {
 	
@@ -57,7 +57,9 @@ public class GameManager {
 			bet();
 		}
 		
-		//getCombination();
+		getCombination(player[0], table);
+		getCombination(player[1], table);
+
 	}
 	
 	public void bet() {
@@ -65,20 +67,85 @@ public class GameManager {
 		//notifyPlayer(player[1], "GAMEINFO_BATTING");
 	}
 	
-	public void getCombination(PlayerDeck pDeck, Cards[] table) {
+	public void getCombination(Player pDeck, Cards[] table) {
 		Cards[] cards = new Cards[7];
-		for(int i = 0; i < 2; i++) cards[i] = pDeck.getDeck(i);
+		for(int i = 0; i < 2; i++) cards[i] = pDeck.getPlayerDeck().getDeck(i+1);
 		for(int i = 0; i < 5; i++) cards[i+2] = table[i];
+		
+		// Combination Top-
 		
 		// 로열 스트레이트 플러쉬
 		for(Cards type : new Cards[] {Cards.DIAMOND, Cards.CLOVER, Cards.SPADE, Cards.HEART}) {
-			if(hasCard(cards, type, "K") && hasCard(cards, type, "Q") && hasCard(cards, type, "J") && hasCard(cards, type, "10") && hasCard(cards, type, "A")) {
+			if(hasCard(cards, type, 13) && hasCard(cards, type, 12) && hasCard(cards, type, 11) && hasCard(cards, type, 10) && hasCard(cards, type, 14)) {
 				// 와! 대박! 로열 스트레이트 플러쉬
 			}
 		}
 		
 		
+		Cards[] combination = {};
+		
 		// 스트레이트 플러쉬
+		if((combination = hasFlush(hasStraight(cards)))[0] != Cards.NO) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "STRAIGHT_FLUSH");
+		}
+		
+		// 4 카드
+		else if((combination = hasMultiple(cards, 4))[0] != Cards.NO) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "FOUR_CARD");
+
+		}
+		
+		// 풀하우스
+		else if((combination = hasFullHouse(cards))[0] != Cards.NO) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "FULL_HOUSE");
+
+		}
+		
+		// 플러쉬
+		else if((combination = hasFlush(cards))[0] != Cards.NO) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "FLUSH");
+
+		}
+		
+		// 스트레이트
+		else if((combination = hasStraight(cards))[0] != Cards.NO) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "STRAIGHT");
+
+		}
+		
+		// 트리플
+		else if((combination = hasMultiple(cards, 3))[0] != Cards.NO) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "TRIPLE");
+
+		}
+		
+		// 투 페어
+		else if((combination = hasMultiple(cards,2))[0] != Cards.NO && combination.length == 4) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "TWO_PAIR");
+
+		}
+		
+		// 원 페어
+		else if((combination = hasMultiple(cards,2))[0] != Cards.NO) {
+			// TODO
+			notifyPlayer(pDeck, combination[0].getNumber() + "ONE_PAIR");
+
+		}
+		
+		// 하이 카드
+		else {
+			// TODO
+			notifyPlayer(pDeck, getTop(cards).getNumber() + "HIGH_CARD");
+
+		}
+		
 		
 	}
 	
@@ -89,14 +156,14 @@ public class GameManager {
 		return false;
 	}
 	
-	public boolean hasCard(Cards[] cards, Cards type, String num) {
+	public boolean hasCard(Cards[] cards, Cards type, int num) {
 		for(int i = 0; i < cards.length; i++) {
 			if(cards[i].getType() == type.getType() && cards[i].getNumber() == num) return true;
 		}
 		return false;
 	}
 	
-	public boolean hasCard(Cards[] cards, String num) {
+	public boolean hasCard(Cards[] cards, int num) {
 		for(int i = 0; i < cards.length; i++) {
 			if(cards[i].getNumber() == num) return true;
 		}
@@ -105,23 +172,88 @@ public class GameManager {
 	
 	public Cards[] hasStraight(Cards[] cards) {
 		Cards[] number = {Cards._2, Cards._3, Cards._4, Cards._5, Cards._6, Cards._7, Cards._8, Cards._9, Cards._10, Cards.J, Cards.Q, Cards.K, Cards.A};
-		int streak;
+		ArrayList<Cards> queue = new ArrayList<Cards>();
 		int currentNumber = number.length - 1;
 		for(int k = currentNumber; k >= 0; k--) {
-			for(int i = 0; i < cards.length; i++) {
-				if(hasCard(cards, number[k].getNumber())) {
-					for(int j = 0; j < 4; j++) {
-						if(k-j > 0 && hasCard(cards, number[k-j].getNumber())) {
-							if(j == 3) {
-								
+			if(hasCard(cards, number[k].getNumber())) {
+				for(int j = 1; j <= 4; j++) {
+					if(k-j > 0 && hasCard(cards, number[k-j].getNumber())) {
+						for(Cards c : cards) {
+							if(c.isSameNumber(number[k-j]) != Cards.NO) {
+								queue.add(c);
 							}
 						}
-					}
+						if(j == 4) {
+							return queue.toArray(new Cards[queue.size()]);
+						}
+					}else break;
 				}
 			}
 		}
 		
-		return null;
+		return new Cards[] {Cards.NO};
 	}
 	
+	public Cards[] hasFlush(Cards[] cards) {
+		Cards[] patterns = {Cards.CLOVER, Cards.DIAMOND, Cards.HEART, Cards.SPADE};
+		ArrayList<Cards> queue = new ArrayList<Cards>();
+		for(Cards pattern : patterns) {
+			int streak = 0;
+			for(Cards c : cards) {
+				if(c.isSameType(pattern) != Cards.NO) {
+					queue.add(c);
+					streak++;
+				}
+			}
+			if(streak >= 5) return queue.toArray(new Cards[queue.size()]);
+			queue.clear();
+		}
+		return new Cards[] {Cards.NO};
+	}
+	
+	public Cards[] hasMultiple(Cards[] cards, int time) {
+		Cards[] numbers = {Cards._2, Cards._3, Cards._4, Cards._5, Cards._6, Cards._7, Cards._8, Cards._9, Cards._10, Cards.J, Cards.Q, Cards.K, Cards.A};
+		ArrayList<Cards> f_Queue = new ArrayList<Cards>();
+		ArrayList<Cards> queue = new ArrayList<Cards>();
+		for(int p = numbers.length - 1; p >= 0; p--) {
+			int streak = 0;
+			for(Cards c : cards) {
+				if(c.getNumber() == numbers[p].getNumber()) {
+					queue.add(c);
+					streak++;
+				}
+			}
+			if(streak == time) f_Queue.addAll(queue);
+			queue.clear();
+		}
+		if(f_Queue.isEmpty()) return new Cards[] {Cards.NO};
+		return f_Queue.toArray(new Cards[f_Queue.size()]);
+	}
+	
+	public Cards[] hasFullHouse(Cards[] cards){
+		ArrayList<Cards> queue = new ArrayList<Cards>();
+		Cards[] triple = hasMultiple(cards, 3);
+		if(triple[0] == Cards.NO) return triple;
+		Cards[] temp = new Cards[cards.length-3];
+		int cursor = 0;
+		for(int i = 0; i < cards.length; i++) {
+			if(cards[i].getNumber() != triple[0].getNumber()) {
+				temp[cursor++] = cards[i]; 
+			}
+		}
+		Cards[] twoPair = hasMultiple(temp, 2);
+		if(twoPair[0] == Cards.NO) return twoPair;
+		return triple;
+	}
+	
+	public Cards getTop(Cards[] cards) {
+		Cards max = cards[0];
+		for(Cards c : cards) {
+			if(c.getNumber() > max.getNumber()) {
+				max = c;
+			}
+		}
+		
+		return max;
+	}
 }
