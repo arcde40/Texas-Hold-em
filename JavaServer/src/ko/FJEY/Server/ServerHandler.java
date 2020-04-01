df60aeec298b79342e6ebaebef4d612f215779ac
FJEY <FJEY@DESKTOP-25B2QUS>
2020-04-01 23:12:07 +0900

    Handler Edit

--- JavaServer/src/ko/FJEY/Server/ServerHandler.java
+++ JavaServer/src/ko/FJEY/Server/ServerHandler.java
@@ -1,14 +1,21 @@
 package ko.FJEY.Server;
 
+import java.util.HashMap;
+
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import ko.FJEY.Game.GameManager;
+import ko.FJEY.Game.Player;
 
 public class ServerHandler extends ChannelInboundHandlerAdapter{
 	private static final ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+	
+	private static Channel waitingChannel;
+	private static HashMap<Channel, String> nickNameMap = new HashMap<Channel, String>();
 	
 	
 	@Override
@@ -42,13 +49,37 @@ public class ServerHandler extends ChannelInboundHandlerAdapter{
 		String s = (String) msg;
 		System.out.println("Channel Read: " + s);
 		
-		for(Channel channel : channelGroup) {
-			if(channel != incoming) {
-				channel.writeAndFlush("["+incoming.remoteAddress()+"] - "+ msg + "\n");
+		if(s.startsWith("CLIENT_JOIN_NICK:")) {
+			String nick = s.substring(17);
+			incoming.writeAndFlush("Oh, Hi! " + nick);
+			System.out.println("Sent Message");
+			nickNameMap.put(incoming, nick);
+			/*if(channelGroup.size() % 2 == 1) {
+				incoming.writeAndFlush("CLIENT_NOT_ENOUGH\n");
+				waitingChannel = incoming;
+				
+			}else */{
+				Channel ch1 = incoming;
+				Channel ch2 = incoming;
+				
+				ch1.writeAndFlush("CLIENT_MATCHED_START\n");
+				ch2.writeAndFlush("CLIENT_MATCHED_START\n");
+				GameManager gm = new GameManager(new Player(ch1, nickNameMap.get(ch1), ch1.remoteAddress().toString()), new Player(ch2, nickNameMap.get(ch2), ch2.remoteAddress().toString()), 0);
+				gm.startGame();
 			}
+			/*for(Channel channel : channelGroup) {
+				//if(channel != incoming) {
+					if(!nickNameMap.containsKey(incoming)) {
+						channel.writeAndFlush("["+incoming.remoteAddress()+"] - "+ msg + "\n");
+					}else {
+						channel.writeAndFlush("["+nickNameMap.get(incoming)+"] - "+ msg + "\n");
+					}
+				//}
+			}*/
+			
+			
 		}
-		
-		incoming.writeAndFlush("[ECHO] " + msg);
+		//incoming.writeAndFlush("[ECHO] " + msg);
 		if("bye".equals(s.toLowerCase())){
 			context.close();
 		}
